af1f64273a1dd69d4d309e66512d30b7a1f7f068
Emanuel Saldivia <emanuel@sima.ag>
2020-12-06 13:26:34 -0300

    app component

---
+++ app/src/main/java/com/esaldivia/melichallenge/BaseApplication.kt
@@ -0,0 +1,13 @@
+package com.esaldivia.melichallenge
+
+import com.esaldivia.melichallenge.di.DaggerAppComponent
+import dagger.android.AndroidInjector
+import dagger.android.support.DaggerApplication
+
+class BaseApplication : DaggerApplication() {
+
+    override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
+        return DaggerAppComponent.builder().application(this).build()
+    }
+
+}
